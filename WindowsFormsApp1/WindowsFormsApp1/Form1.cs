9e7de30d15673c956a826487bb717cf8697b329a
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2019-09-24 12:12:30 +0200

    Add files via upload
    drugi zadatak

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int broj = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            broj--;
+            txtPrikaz.Text = broj.ToString();
+        }
+
+        private void BtnPlus_Click(object sender, EventArgs e)
+        {
+            broj++;
+            txtPrikaz.Text = broj.ToString();
+        }
+
+        private void TxtPrikaz_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
